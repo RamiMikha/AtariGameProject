e4608dea5f601592b2b874cb2e088418c78f6fe0
Rami Mikha <ramikmikha@gmail.com>
2024-02-01 20:51:17 -0700

    Implemented plot_bitmap_32

--- raster/raster.c
+++ raster/raster.c
@@ -1,1 +1,16 @@
+#include "raster.h"
+
+void plot_bitmap_32(ULONG32 *base, int x, int y, ULONG32 *bitmap, unsigned int height)
+{
+    int screenWidth = 20;
+
+	int offset = (y * screenWidth) + (x/32);
+
+	int i;
+	for (i = 0; i < height; i++) {
+		*(base + offset) |= bitmap[i]; 
+		offset += screenWidth;
+	}
+
+}
 
