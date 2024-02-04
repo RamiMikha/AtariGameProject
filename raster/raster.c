4e86753a8a4a840abfe80432fd5ae19648510905
Rami Mikha <ramikmikha@gmail.com>
2024-02-03 18:17:19 -0700

    Implemented New Functions and Error Checking
    Implemented plot_bitmap_8 and fill_region. Added error checking to the other two functions

--- raster/raster.c
+++ raster/raster.c
@@ -3,15 +3,35 @@
 void plot_bitmap_32(UINT32 *base, int x, int y, UINT32 *bitmap, unsigned int height)
 {
     int screenWidth = 20;
-
-	int offset = (y * screenWidth) + (x/32);
+	int longs_per_screen = 8000;
+	int offset = (y * screenWidth) + (x >> 5);
 
 	int i;
 	for (i = 0; i < height; i++) {
-		*(base + offset) |= bitmap[i]; 
+		if (offset < longs_per_screen)
+		{
+			*(base + offset) |= bitmap[i];
+		}
 		offset += screenWidth;
 	}
 
+}
+
+void plot_bitmap_8(UINT8 *base, int x, int y, UINT8 *bitmap, unsigned int height)
+{
+	int screenWidth = 80;
+	int bytes_per_screen = 32000;
+
+	int offset = (y * screenWidth) + (x >> 3);
+
+	int i;
+	for (i = 0; i < height; i++) {
+		if (offset < bytes_per_screen)
+		{
+			*(base + offset) |= bitmap[i];
+		}
+		offset += screenWidth;
+	}
 }
 
 void clear_screen(UINT32 *base){
@@ -20,11 +40,33 @@ void clear_screen(UINT32 *base){
   	UINT32 *offset = base;
   	int x = 0;
 
-  	while (x++ <longs_per_screen)
+  	while (x++ < longs_per_screen)
   	{
     	*(offset++) = 0;
   	}	
 
 }
 
+
+
+void fill_region(UINT32 *base, int x1, int x2, int y1, int y2)
+{
+	int screenWidth = 20;
+	
+	int j = y1;
+	
+	
+	while (j < y2 && j < 400)
+	{
+		int i = x1;
+		while (i < x2 && i < 640)
+		{
+			int offset = (j * screenWidth) + (x1/32);
+			*(base + offset) = 1;
+			i++;
+		}
+		j++;
+	}
+
+}
 
