3815aed1f968a027b673025e7b7fbc7fce6c1a14
Rami Mikha <ramikmikha@gmail.com>
2024-02-03 17:54:21 -0700

    Implemented Rest of Summary
    Implemented plot_bitmap_8 and fill_region

--- raster/raster.c
+++ raster/raster.c
@@ -4,7 +4,7 @@ void plot_bitmap_32(UINT32 *base, int x, int y, UINT32 *bitmap, unsigned int hei
 {
     int screenWidth = 20;
 
-	int offset = (y * screenWidth) + (x/32);
+	int offset = (y * screenWidth) + (x >> 5);
 
 	int i;
 	for (i = 0; i < height; i++) {
@@ -14,17 +14,51 @@ void plot_bitmap_32(UINT32 *base, int x, int y, UINT32 *bitmap, unsigned int hei
 
 }
 
+void plot_bitmap_8(UINT8 *base, int x, int y, UINT8 *bitmap, unsigned int height)
+{
+	int screenWidth = 80;
+
+	int offset = (y * screenWidth) + (x >> 3);
+
+	int i;
+	for (i = 0; i < height; i++) {
+		*(base + offset) |= bitmap[i]; 
+		offset += screenWidth;
+	}
+}
+
 void clear_screen(UINT32 *base){
 
 	int longs_per_screen = 8000;
   	UINT32 *offset = base;
   	int x = 0;
 
-  	while (x++ <longs_per_screen)
+  	while (x++ < longs_per_screen)
   	{
     	*(offset++) = 0;
   	}	
 
 }
 
+
+
+void fill_region(UINT32 *base, int x1, int x2, int y1, int y2)
+{
+	int screenWidth = 20;
+	int j = y1;
+	
+
+	while (j < y2)
+	{
+		int i = x1;
+		while (i < x2)
+		{
+			int offset = (j * screenWidth) + (x1/32);
+			*(base + offset) = 1;
+			i++;
+		}
+		j += screenWidth;
+	}
+
+}
 
--- raster/raster.h
+++ raster/raster.h
@@ -4,6 +4,7 @@
 #include "types.h"
 
 void plot_bitmap_32(UINT32 *base, int x, int y, UINT32 *bitmap, unsigned int height);
+void plot_bitmap_8(UINT8 *base, int x, int y, UINT8 *bitmap, unsigned int height);
 void clear_screen(UINT32 *base);
 void fill_region(UINT32 *base, int x1, int x2, int y1, int y2);
 
