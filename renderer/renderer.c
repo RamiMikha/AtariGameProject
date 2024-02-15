03f53a05042704eb7c4fb514a55e09ce2245c23c
Jarod Dacocos <JarodDacocos@gmail.com>
2024-02-15 14:29:52 -0700

    fixed parameter order

--- renderer/renderer.c
+++ renderer/renderer.c
@@ -1,21 +1,23 @@
 #include "renderer.h"
 
 
-void render();
+void render(UINT32 *base, Model *model) {
+    render_bird();
+    render_pipe();
+    render_ground();
+}
 
-void render_bird(Bird *bird, UINT32 *base)  
-{
+
+void render_bird(UINT32 *base, Bird *bird) {
     plot_bitmap_32(base, bird->x, bird->y, flappy_bitmap, BIRD_HEIGHT);
 }
 
-void render_pipe(Pipes *pipe, UINT32 *base)
-{
+void render_pipe(UINT32 *base, Pipes *pipe) {
     /*possible logic: if we have the pipe bitmap be smaller squares of the pipe
     we can have a while loop to print the parts until reaching pipe->opening_height
     then it prints the opening(lines of zeros) then prints more pipe square parts until it reacher ground */
 
-    while(pipe->y < GROUND_Y1) /*To do: decide on ground y*/
-    {
+    while(pipe->y < GROUND_Y1) { /*To do: decide on ground y*/
         plot_bitmap_32(base, pipe->x, pipe->y, pipe_section_bitmap, PIPE_SECTION_HEIGHT); /*To do: decide on section height and create bitmap*/
         pipe->y += PIPE_SECTION_HEIGHT;
     }
@@ -24,13 +26,11 @@ void render_pipe(Pipes *pipe, UINT32 *base)
 
 }
 
-void render_pipe_opening(UINT32 *base, int opening_size, int x, int y)
-{
+void render_pipe_opening(UINT32 *base, int opening_size, int x, int y) {
     fill_region(base, x, x + PIPE_WIDTH, y, y + opening_size, 0); 
 }
 
-void render_ground(UINT32 *base, const Ground *ground) 
-{
+void render_ground(UINT32 *base, const Ground *ground) {
     fill_region(base, ground->x1, ground->x2, ground->y1, ground->y2, 1);
 }
 
