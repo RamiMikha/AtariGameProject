63ab237221bc7d10300302a1f30729c309451060
Rami Mikha <ramikmikha@gmail.com>
2024-02-14 21:12:44 -0700

    Implemented render_bird and render_ground

--- renderer/renderer.c
+++ renderer/renderer.c
@@ -1,1 +1,30 @@
-#include renderer.h
+#include "renderer.h"
+#include "model.h"
+#include "raster.h"
+
+
+void render();
+
+void render_bird(const Bird *bird, UINT32 *base)  
+{
+    plot_bitmap_32(base, bird->x, bird->y, flappy_bitmap, BIRD_HEIGHT);
+}
+
+void render_pipe(const Pipes *pipe, UINT32 *base)
+{
+    /*possible logic: if we have the pipe bitmap be smaller squares of the pipe
+    we can have a while loop to print the parts until reaching pipe->opening_height
+    then it prints the opening(lines of zeros) then prints more pipe square parts until it reacher ground */
+}
+
+void render_pipe_opening();
+
+void render_ground(UINT32 *base, const Ground *ground) 
+{
+    fill_region(base, ground->x1, ground->x2, ground->y1, ground->y2);
+}
+{
+
+}
+
+void render_score();
--- renderer/renderer.h
+++ renderer/renderer.h
@@ -1,6 +1,41 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
+const UINT32 flappy_bitmap[32] = {
+  0x00000000, 
+  0x00000000, 
+  0x00000000, 
+  0x000fff00, 
+  0x000fff00, 
+  0x00f060c0, 
+  0x00f060c0, 
+  0x03018030, 
+  0x03018030, 
+  0x7fc180cc, 
+  0x7fc180cc, 
+  0x806180cc, 
+  0x806180cc, 
+  0x8018600c, 
+  0x8018600c, 
+  0x80181ffe, 
+  0x80181ffe, 
+  0x60606001, 
+  0x60606001, 
+  0x1f819ffe, 
+  0x1f819ffe, 
+  0x18006006, 
+  0x18006006, 
+  0x07801ff8, 
+  0x07801ff8, 
+  0x007fe000, 
+  0x007fe000, 
+  0x00000000, 
+  0x00000000, 
+  0x00000000,
+  0x00000000, 
+  0x00000000,
+};
+
 void render();
 
 void render_bird();
@@ -10,5 +45,7 @@ void render_pipe();
 void render_pipe_opening();
 
 void render_ground();
+
+void render_score();
 
 #endif
