c5493c9e707932990c89212a9c2738abbb282421
Rami Mikha <ramikmikha@gmail.com>
2024-02-14 21:38:49 -0700

    Updated fill_region

--- raster/raster.c
+++ raster/raster.c
@@ -38,7 +38,7 @@ void clear_screen(UINT32 *base) {
   	}	
 }
 
-void fill_region(UINT32 *base, int x1, int x2, int y1, int y2)
+void fill_region(UINT32 *base, int x1, int x2, int y1, int y2, int color)
 {
 	int screenWidth = 20;
 	int j = y1;
@@ -47,7 +47,14 @@ void fill_region(UINT32 *base, int x1, int x2, int y1, int y2)
 		int i = x1;
 		while (i < x2 && i < 640) {
 			int offset = (j * screenWidth) + (i/32);
-			*(base + offset) = 0xFFFFFFFF;
+			if (color == 0){
+				*(base + offset) = 0x00000000;
+			}
+			else
+			{
+				*(base + offset) = 0xFFFFFFFF;
+			}
+			
 			i++;
 		}
 		j++;
