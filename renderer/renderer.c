a86e3d647bcc19b5c9c42a70f38ade8459025ef3
Rami Mikha <ramikmikha@gmail.com>
2024-02-16 17:16:49 -0700

    Moved Bitmaps from .h to .c

--- renderer/renderer.c
+++ renderer/renderer.c
@@ -1,5 +1,76 @@
 #include "renderer.h"
 
+const UINT32 flappy_bitmap[BIRD_HEIGHT] = {
+  0x00000000, 
+  0x00000000, 
+  0x00000000, 
+  0x000fff00, 
+  0x000fff00, 
+  0x00f060c0, 
+  0x00f060c0, 
+  0x03018030, 
+  0x03018030, 
+  0x7fc180cc, 
+  0x7fc180cc, 
+  0x806180cc, 
+  0x806180cc, 
+  0x8018600c, 
+  0x8018600c, 
+  0x80181ffe, 
+  0x80181ffe, 
+  0x60606001, 
+  0x60606001, 
+  0x1f819ffe, 
+  0x1f819ffe, 
+  0x18006006, 
+  0x18006006, 
+  0x07801ff8, 
+  0x07801ff8, 
+  0x007fe000, 
+  0x007fe000, 
+  0x00000000, 
+  0x00000000, 
+  0x00000000,
+  0x00000000, 
+  0x00000000
+};
+
+const UINT32 pipe_section_bitmap[PIPE_SECTION_HEIGHT] = {
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF,
+   0xFFFFFFFF
+
+};
+
 void render(UINT32 *base, Model model) {
     clear_screen(base);
     render_bird(base, &model.bird);
--- renderer/renderer.h
+++ renderer/renderer.h
@@ -17,76 +17,7 @@ pipe.
 #include "..\model.h"
 #include "..\raster\raster.h"
 
-const UINT32 flappy_bitmap[BIRD_HEIGHT] = {
-  0x00000000, 
-  0x00000000, 
-  0x00000000, 
-  0x000fff00, 
-  0x000fff00, 
-  0x00f060c0, 
-  0x00f060c0, 
-  0x03018030, 
-  0x03018030, 
-  0x7fc180cc, 
-  0x7fc180cc, 
-  0x806180cc, 
-  0x806180cc, 
-  0x8018600c, 
-  0x8018600c, 
-  0x80181ffe, 
-  0x80181ffe, 
-  0x60606001, 
-  0x60606001, 
-  0x1f819ffe, 
-  0x1f819ffe, 
-  0x18006006, 
-  0x18006006, 
-  0x07801ff8, 
-  0x07801ff8, 
-  0x007fe000, 
-  0x007fe000, 
-  0x00000000, 
-  0x00000000, 
-  0x00000000,
-  0x00000000, 
-  0x00000000
-};
 
-const UINT32 pipe_section_bitmap[PIPE_SECTION_HEIGHT] = {
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF,
-   0xFFFFFFFF
-
-};
 
 /*------------------------------------------------------------                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 Function Name: render                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
