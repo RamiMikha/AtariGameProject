46bc989fc0ef7c0bd36d47eeabb72e551d6059b3
Rami Mikha <ramikmikha@gmail.com>
2024-02-24 18:55:28 -0700

    Updated Renderer
    will now only rerender objects if they move

--- TST_REN.c
+++ TST_REN.c
@@ -8,9 +8,15 @@ int main() {
     model.bird.x = 100;
     model.bird.y = 100;
     model.pipe.x = 300;
+    model.pipe.y = 0;
     model.score.value = 10;
 
-    /*Calling main render function to test the rendering of the full screen*/
+    clear_screen(base);
+    render(base, base8, model);
+    model.bird.prev_y = model.bird.y;
+    model.pipe.prev_x = model.pipe.x;
+    model.bird.y = 200;
+    model.pipe.x = 400;
     render(base, base8, model);
     return 0;
 
--- renderer/renderer.c
+++ renderer/renderer.c
@@ -1,11 +1,20 @@
 #include "renderer.h"
 #include "../bitmaps.h"
+#include <stdio.h>
 
 
 void render(UINT32 *base, UINT8 *base8, Model model) {
-    clear_screen(base);
-    render_bird(base, &model.bird);
-    render_pipe(base, &model.pipe);
+    
+    if(model.bird.y != model.bird.prev_y) {
+        clear_bird(base, &model.bird);
+        render_bird(base, &model.bird);
+        model.bird.prev_y = model.bird.y;
+    }
+    if(model.pipe.x != model.pipe.prev_x) {
+        clear_pipe(base, &model.pipe);
+        render_pipe(base, &model.pipe);
+        model.pipe.prev_x = model.pipe.x;
+    }
     render_ground(base);
     render_score(base8, 10, SCORE_Y, &model.score);
 }
@@ -15,7 +24,12 @@ void render_bird(UINT32 *base, Bird *bird) {
     plot_bitmap_32(base, bird->x, bird->y, flappy_bitmap, BIRD_HEIGHT);
 }
 
+void clear_bird(UINT32 *base, Bird *bird) {
+    fill_region(base, bird->x, bird->x + BIRD_WIDTH, bird->prev_y, bird->prev_y + BIRD_HEIGHT, 0);
+}
+
 void render_pipe(UINT32 *base, Pipes *pipe) {
+    pipe->y = 0;
     while(pipe->y < GROUND_Y1) { 
         plot_bitmap_32(base, pipe->x, pipe->y, pipe_section_bitmap, PIPE_SECTION_HEIGHT);
         pipe->y += PIPE_SECTION_HEIGHT;
@@ -29,6 +43,14 @@ void render_pipe(UINT32 *base, Pipes *pipe) {
 
 void render_pipe_opening(UINT32 *base, int opening_size, int x, int y) {
     fill_region(base, x, x + PIPE_WIDTH, y, y + opening_size, 0); 
+}
+
+void clear_pipe(UINT32 *base, Pipes *pipe) {
+    pipe->y = 0;
+    while (pipe->y < GROUND_Y1) {
+        fill_region(base, pipe->prev_x, pipe->prev_x + PIPE_WIDTH, pipe->y, pipe->y + PIPE_SECTION_HEIGHT, 0);
+        pipe->y += PIPE_SECTION_HEIGHT;
+    }
 }
 
 void render_ground(UINT32 *base) {
