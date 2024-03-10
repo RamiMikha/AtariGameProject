a022060aad26b9a0f743962776e22390dfed97b0
Jarod Dacocos <JarodDacocos@gmail.com>
2024-03-10 09:54:15 -0600

    Added sound effect functions to library

--- music/psg.h
+++ music/psg.h
@@ -1,9 +1,13 @@
 #ifndef PSG_H
 #define PSG_H
 
+#include <osbind.h>
+
+
+/* Main PSG Routines */
 void write_psg (int reg, UINT8 val);
 
- /* UINT8 read_psg (int reg); optional */
+UINT8 read_psg (int reg);
 
 void set_tone (int channel, int tuning);
 
@@ -11,6 +15,11 @@ void set_volume (int channel, int volume);
 
 void enable_channel (int channel, int tone_on, int noise_on);
 
-void stop_sound();
+void stop_sound ();
+
+/* Sound Effects */
+void set_noise (int tuning);
+
+void set_envelope (int shape, unsigned int sustain);
 
 #endif
