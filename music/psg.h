28986e6276a1fa2cbbcd8a738824266a7f4ef653
Jarod Dacocos <JarodDacocos@gmail.com>
2024-03-10 09:19:31 -0600

    Update psg.h

--- music/psg.h
+++ music/psg.h
@@ -1,1 +1,16 @@
+#ifndef PSG_H
+#define PSG_H
 
+void write_psg (int reg, UINT8 val);
+
+ /* UINT8 read_psg (int reg); optional */
+
+void set_tone (int channel, int tuning);
+
+void set_volume (int channel, int volume);
+
+void enable_channel (int channel, int tone_on, int noise_on);
+
+void stop_sound();
+
+#endif
