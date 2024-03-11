f7197b1a93824ca4a23549cda13579595888a9a7
Rami Mikha <ramikmikha@gmail.com>
2024-03-10 23:14:59 -0600

    Made Letter Bitmaps

--- bitmaps.h
+++ bitmaps.h
@@ -17,5 +17,32 @@ extern const UINT8 six_bitmap[FONT_LENGTH];
 extern const UINT8 seven_bitmap[FONT_LENGTH];
 extern const UINT8 eight_bitmap[FONT_LENGTH];
 extern const UINT8 nine_bitmap[FONT_LENGTH];
+extern const UINT8 A_bitmap[FONT_LENGTH];
+extern const UINT8 B_bitmap[FONT_LENGTH];
+extern const UINT8 C_bitmap[FONT_LENGTH];
+extern const UINT8 D_bitmap[FONT_LENGTH];
+extern const UINT8 E_bitmap[FONT_LENGTH];
+extern const UINT8 F_bitmap[FONT_LENGTH];
+extern const UINT8 G_bitmap[FONT_LENGTH];
+extern const UINT8 H_bitmap[FONT_LENGTH];
+extern const UINT8 I_bitmap[FONT_LENGTH];
+extern const UINT8 J_bitmap[FONT_LENGTH];
+extern const UINT8 K_bitmap[FONT_LENGTH];
+extern const UINT8 L_bitmap[FONT_LENGTH];
+extern const UINT8 M_bitmap[FONT_LENGTH];
+extern const UINT8 N_bitmap[FONT_LENGTH];
+extern const UINT8 O_bitmap[FONT_LENGTH];
+extern const UINT8 P_bitmap[FONT_LENGTH];
+extern const UINT8 Q_bitmap[FONT_LENGTH];
+extern const UINT8 R_bitmap[FONT_LENGTH];
+extern const UINT8 S_bitmap[FONT_LENGTH];
+extern const UINT8 T_bitmap[FONT_LENGTH];
+extern const UINT8 U_bitmap[FONT_LENGTH];
+extern const UINT8 V_bitmap[FONT_LENGTH];
+extern const UINT8 W_bitmap[FONT_LENGTH];
+extern const UINT8 X_bitmap[FONT_LENGTH];
+extern const UINT8 Y_bitmap[FONT_LENGTH];
+extern const UINT8 Z_bitmap[FONT_LENGTH];
+
 
 #endif
