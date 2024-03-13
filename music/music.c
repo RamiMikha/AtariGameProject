7be069a9db1bf2521db7d860409d34fb54a63faf
Rami Mikha <ramikmikha@gmail.com>
2024-03-12 21:11:20 -0600

    Bug Fixing

--- music/music.c
+++ music/music.c
@@ -12,9 +12,9 @@ UINT32 NOTE_DURATION = 1000;
 
 void start_music(Music *music){
     music->current_note = 0;
-    music->channel = &song[music->current_note].channel;
-    music->tune = &song[music->current_note].tune;
-    music->volume = &song[music->current_note].volume;
+    music->channel = song[music->current_note].channel;
+    music->tune = song[music->current_note].tune;
+    music->volume = song[music->current_note].volume;
 
     set_tone(music->channel, music->tune);
     set_volume(music->channel, music->volume);
@@ -27,14 +27,20 @@ void update_music(UINT32 time_elapsed, Music *music){
     
     time_since_last_note += time_elapsed;
     if (time_since_last_note > NOTE_DURATION){
-        music->current_note++;
-        music->channel = &song[music->current_note].channel;
-        music->tune = &song[music->current_note].tune;
-        music->volume = &song[music->current_note].volume;
+        if (music->current_note < sizeof(song)/sizeof(song[0])){
+            music->current_note++;
+            music->channel = song[music->current_note].channel;
+            music->tune = song[music->current_note].tune;
+            music->volume = song[music->current_note].volume;
+            set_tone(music->channel, music->tune);
+            set_volume(music->channel, music->volume);
+            enable_channel(music->channel, 1, 0);
+        } else {
+            start_music(music);
+        }
 
-        set_tone(music->channel, music->tune);
-        set_volume(music->channel, music->volume);
-        enable_channel(music->channel, 1, 0);
+
+
 
         time_since_last_note = 0;
     }
