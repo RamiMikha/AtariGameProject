1df428d04dde1abcafa16fde651345a5ea7fcf7b
Rami Mikha <ramikmikha@gmail.com>
2024-03-12 21:47:18 -0600

    Update music.c

--- music/music.c
+++ music/music.c
@@ -1,12 +1,13 @@
 #include "music.h"
+#include <stdio.h>
 
 extern Music song[]= {
-    {0, 248, 11},
-    {0, 249, 10},
-    {0, 250, 11}
+    {0, 248, 50},
+    {0, 249, 50},
+    {0, 250, 50}
 };
 
-UINT32 NOTE_DURATION = 1000;
+UINT32 NOTE_DURATION = 100;
 
 /*No function is tested yet*/
 
@@ -23,8 +24,8 @@ void start_music(Music *music){
 
 
 void update_music(UINT32 time_elapsed, Music *music){
-    int time_since_last_note = 0;
-    
+    int time_since_last_note;
+
     time_since_last_note += time_elapsed;
     if (time_since_last_note > NOTE_DURATION){
         if (music->current_note < sizeof(song)/sizeof(song[0])){
@@ -35,13 +36,10 @@ void update_music(UINT32 time_elapsed, Music *music){
             set_tone(music->channel, music->tune);
             set_volume(music->channel, music->volume);
             enable_channel(music->channel, 1, 0);
+
         } else {
             start_music(music);
         }
-
-
-
-
         time_since_last_note = 0;
-    }
+    }   
 }
