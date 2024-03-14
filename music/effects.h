66e048650c178545ae1b41c59969b6124b34d09d
Rami Mikha <ramikmikha@gmail.com>
2024-03-13 21:20:33 -0600

    Update effects.h

--- music/effects.h
+++ music/effects.h
@@ -10,10 +10,10 @@ struct pass_pipe_effect
 
 struct collision_effect
 {
-    
+
 };
 
 
 void play_bird_effect();
 void play_pipe_effect();
-void _play_collision_effect();
+void play_collision_effect();
