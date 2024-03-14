5cb430819e54473175400735346eda8ca3e7014d
Rami Mikha <ramikmikha@gmail.com>
2024-03-13 20:58:48 -0600

    Fixed PSG Functions

--- music/psg.c
+++ music/psg.c
@@ -1,9 +1,9 @@
 #include "psg.h"
 
-long old_ssp;
 void write_psg (int reg, UINT8 val) {
   volatile char *PSG_reg_select = 0xFF8800;
   volatile char *PSG_reg_write = 0xFF8802;
+  UINT32 old_ssp;
   
   old_ssp = Super(0);
 
@@ -14,13 +14,19 @@ void write_psg (int reg, UINT8 val) {
 }
 
 UINT8 read_psg(int reg) {
-  /* For testing purposes */
-  UINT8 val;
+    volatile char *PSG_reg_select = (char *)0xFF8800;
+    volatile char *PSG_reg_read = (char *)0xFF8802;
+    UINT32 old_ssp;
+    UINT8 val;
 
-  write_psg(reg, 0);
-  val = *((volatile char *)0xFF8802);
+    old_ssp = Super(0);
 
-  return val;
+    *PSG_reg_select = reg;
+    val = *PSG_reg_read;
+
+    Super(old_ssp);
+
+    return val;
 }
 
 void set_tone (int channel, int tuning) {
@@ -34,23 +40,37 @@ void set_tone (int channel, int tuning) {
 }
 
 void set_volume (int channel, int volume) {
-  write_psg(channel, volume);
+  write_psg(8 + channel, volume);
 }
 
 void enable_channel (int channel, int tone_on, int noise_on) {
   UINT8 reg_value = 0;
 
+  reg_value = read_psg(7);
+
   if (tone_on) {
+    reg_value &= ~(1 << channel);
+  }
+  else {
     reg_value |= (1 << channel);
   }
 
   if (noise_on) {
+    reg_value &= ~(1 << (channel + 3));
+  }
+  else {
     reg_value |= (1 << (channel + 3));
   }
 
-  write_psg (channel, reg_value);
+  write_psg (7, reg_value);
 }
 
 void stop_sound() {
-  Super(old_ssp);
+  UINT32 old_ssp;
+  int channel;
+  for (channel = 0; channel < 3; channel++) {
+    set_volume(channel, 0);
+    enable_channel(channel, 0, 0);
+  }
+
 }
