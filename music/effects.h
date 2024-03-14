05024f92f78cb84dedea7b3fcbecbc169d0df9ed
Rami Mikha <ramikmikha@gmail.com>
2024-03-13 21:20:10 -0600

    Update effects.h

--- music/effects.h
+++ music/effects.h
@@ -1,1 +1,19 @@
+struct flappy_effect
+{
 
+};
+
+struct pass_pipe_effect
+{
+
+};
+
+struct collision_effect
+{
+    
+};
+
+
+void play_bird_effect();
+void play_pipe_effect();
+void _play_collision_effect();
