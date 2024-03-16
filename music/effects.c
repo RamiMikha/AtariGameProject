f345f7350e60cb32e60d241a4575b8fef7bcb718
Rami Mikha <ramikmikha@gmail.com>
2024-03-15 18:33:36 -0600

    Updated Music and Effects

--- music/effects.c
+++ music/effects.c
@@ -1,22 +1,48 @@
 #include "effects.h"
 
 Sound flap_noise[] = {
-    {1, 2000, 5}
+    {2, 0x281, 10},
+    {2, 0x1C5, 10},
+    {2, 0x2A7, 10},
+    {2, 0x281, 10},
+    {2, 0x1C5, 10},
+    {2, 0x2A7, 10},
+    {2, 0x281, 10},
+    {2, 0x1C5, 10},
+    {2, 0x2A7, 10},
+    {2, 0x281, 10},
+    {2, 0x1C5, 10},
+    {2, 0x2A7, 10}    
 };
 
 Sound ping_sound[] = {
-    {2, 4000, 10}
+    {2, 0x01B, 10},
+    {2, 0x01B, 10},
+    {2, 0x01B, 10},
+    {2, 0x01B, 10},
+    {2, 0x01B, 10},
+    {2, 0x01B, 10},
+    {2, 0x01B, 10},
+    {2, 0x01B, 10},
 };
 
 Sound hit_sound[] = {
-    {2, 500, 10}
+    {2, 300, 10},
+    {2, 300, 10},
+    {2, 300, 10},
+    {2, 300, 10},
+    {2, 300, 10},
+    {2, 300, 10},
+    {2, 300, 10}
 };
 void play_bird_effect(){
     int i;
     for (i = 0; i < sizeof(flap_noise)/sizeof(flap_noise[0]); i++){
-        set_tone(flap_noise[i].channel, flap_noise[i].tune);
+        set_tone(flap_noise[i].channel, flap_noise[i].tone);
         set_volume(flap_noise[i].channel, flap_noise[i].volume);
-        enable_channel(flap_noise[i].channel, 1, 1);
+        set_noise(flap_noise[i].tone);
+        set_envelope(0, 3500);
+        enable_channel(flap_noise[i].channel, 1, 0);
         enable_channel(flap_noise[i].channel, 0, 0);
     }
     
@@ -25,18 +51,20 @@ void play_bird_effect(){
 void play_pipe_effect(){
     int i;
     for (i = 0; i < sizeof(ping_sound)/sizeof(ping_sound[0]); i++){
-        set_tone(ping_sound[i].channel, ping_sound[i].tune);
+        set_tone(ping_sound[i].channel, ping_sound[i].tone);
         set_volume(ping_sound[i].channel, ping_sound[i].volume);
-        enable_channel(ping_sound[i].channel, 1, 1);
+        set_envelope(0, 3500);
+        enable_channel(ping_sound[i].channel, 1, 0);
         enable_channel(ping_sound[i].channel, 0, 0);
     }
 }
 void play_collision_effect(){
     int i;
     for (i = 0; i < sizeof(hit_sound)/sizeof(hit_sound[0]); i++){
-        set_tone(hit_sound[i].channel, hit_sound[i].tune);
+        set_tone(hit_sound[i].channel, hit_sound[i].tone);
         set_volume(hit_sound[i].channel, hit_sound[i].volume);
-        enable_channel(hit_sound[i].channel, 1, 1);
+        set_envelope(0, 3500);
+        enable_channel(hit_sound[i].channel, 1, 0);
         enable_channel(hit_sound[i].channel, 0, 0);
     }
 }
--- music/music.c
+++ music/music.c
@@ -4,34 +4,44 @@
 #include <stdlib.h>
 
 Note song[]= {
-    {0, 60, 100},
-    {0, 60, 100},
-    {0, 67, 100},
-    {0, 67, 100},
-    {0, 69, 100},
-    {0, 69, 100},
-    {0, 67, 100},
-    {0, 65, 100},
-    {0, 64, 100},
-    {0, 62, 100},
-    {0, 60, 100}
+    {0, 0x1AC, 10},
+    {0, 0x17D, 10},
+    {0, 0x1AC, 10},
+    {0, 0x140, 10},
+    {0, 0x153, 10},
+    {0, 0x1AC, 10},
+    {0, 0x17D, 10},
+    {0, 0x1AC, 10},
+    {0, 0x11D, 10},
+    {0, 0x140, 10},
+    {0, 0x1AC, 10},
+    {0, 0x1AC, 10},
+    {0, 0x0FE, 10},
+    {0, 0x140, 10},
+    {0, 0x153, 10},
+    {0, 0x17D, 10},
+    {0, 0x0E2, 10},
+    {0, 0x0FE, 10},
+    {0, 0x140, 10},
+    {0, 0x11D, 10},
+    {0, 0x140, 10}
 };
 
-UINT32 NOTE_DURATION = 10;
+UINT32 NOTE_DURATION = 20;
 Music* music;
 
 void start_music(){
-    int channel, tune, volume;
+    int channel, tone, volume;
     music = (Music*)malloc(sizeof(Music));
     music->current_note = 0;
     music->song = song;
     channel = music->song[music->current_note].channel;
-    tune = music->song[music->current_note].tune;
+    tone = music->song[music->current_note].tone;
     volume = music->song[music->current_note].volume;
 
-    set_tone(channel, tune);
+    set_tone(channel, tone);
     set_volume(channel, volume);
-    enable_channel(channel, 1, 1);
+    enable_channel(channel, 1, 0);
 }
 
 
@@ -40,15 +50,15 @@ void update_music(UINT32 time_elapsed){
     music->time_since_last_note += time_elapsed;
     if (music->time_since_last_note > NOTE_DURATION){
         if (music->current_note < sizeof(song)/sizeof(song[0])){
-            int channel, tune, volume;
+            int channel, tone, volume;
             music->current_note++;
             channel = music->song[music->current_note].channel;
-            tune = music->song[music->current_note].tune;
+            tone = music->song[music->current_note].tone;
             volume = music->song[music->current_note].volume;
 
-            set_tone(channel, tune);
+            set_tone(channel, tone);
             set_volume(channel, volume);
-            enable_channel(channel, 1, 1);
+            enable_channel(channel, 1, 0);
 
         } else {
             start_music();
