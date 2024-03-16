4ca418edf60f499c1e7ed4c0b368255ef98c82d0
Jarod Dacocos <JarodDacocos@gmail.com>
2024-03-15 19:44:41 -0600

    Update bitmaps.h

--- bitmaps.h
+++ bitmaps.h
@@ -7,6 +7,8 @@
 extern const UINT32 flappy_bitmap[BIRD_HEIGHT];
 extern const UINT32 flappy_middle_bitmap[BIRD_HEIGHT];
 extern const UINT32 flappy_down_bitmap[BIRD_HEIGHT];
+const UINT32 flappy_bitmaps[];
+
 extern const UINT32 pipe_section_bitmap[PIPE_SECTION_HEIGHT];
 
 extern const UINT8 zero_bitmap[FONT_LENGTH]; 
