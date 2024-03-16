7ab6f417915e167c5fcfd0da73ae920ffbde8fcc
Rami Mikha <ramikmikha@gmail.com>
2024-03-15 18:58:51 -0600

    Update effects.c

--- music/effects.c
+++ music/effects.c
@@ -16,14 +16,10 @@ Sound flap_noise[] = {
 };
 
 Sound ping_sound[] = {
-    {2, 0x01B, 10},
-    {2, 0x01B, 10},
-    {2, 0x01B, 10},
-    {2, 0x01B, 10},
-    {2, 0x01B, 10},
-    {2, 0x01B, 10},
-    {2, 0x01B, 10},
-    {2, 0x01B, 10},
+    {2, 0x024, 20},
+    {2, 0x02A, 20},
+    {2, 0x028, 20},
+    {2, 0x035, 20}
 };
 
 Sound hit_sound[] = {
@@ -35,6 +31,12 @@ Sound hit_sound[] = {
     {2, 300, 10},
     {2, 300, 10}
 };
+
+void delay(int milliseconds) {
+    int i, j;
+    for(i = 0; i < milliseconds; i++){} 
+}
+
 void play_bird_effect(){
     int i;
     for (i = 0; i < sizeof(flap_noise)/sizeof(flap_noise[0]); i++){
@@ -43,7 +45,7 @@ void play_bird_effect(){
         set_noise(flap_noise[i].tone);
         set_envelope(0, 3500);
         enable_channel(flap_noise[i].channel, 1, 0);
-        enable_channel(flap_noise[i].channel, 0, 0);
+        enable_channel(flap_noise[i].channel, 0, 0);        
     }
     
 
@@ -55,6 +57,7 @@ void play_pipe_effect(){
         set_volume(ping_sound[i].channel, ping_sound[i].volume);
         set_envelope(0, 3500);
         enable_channel(ping_sound[i].channel, 1, 0);
+        delay(500);
         enable_channel(ping_sound[i].channel, 0, 0);
     }
 }
@@ -65,6 +68,7 @@ void play_collision_effect(){
         set_volume(hit_sound[i].channel, hit_sound[i].volume);
         set_envelope(0, 3500);
         enable_channel(hit_sound[i].channel, 1, 0);
+        delay(500);
         enable_channel(hit_sound[i].channel, 0, 0);
     }
 }
