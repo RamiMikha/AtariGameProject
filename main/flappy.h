ea6f0e875b1284a4e00775824c836b1c0ad42bf3
Rami Mikha <ramikmikha@gmail.com>
2024-03-28 00:24:21 -0600

    Created flappy.h

---
+++ main/flappy.h
@@ -0,0 +1,18 @@
+#include "..\renderer\renderer.h"
+#include "..\events\events.h"
+#include "..\raster\types.h"
+#include "input.h"
+#include "..\music\music.h"
+#include "..\music\effects.h"
+#include <osbind.h>
+#include "..\raster\raster.h"
+
+#ifndef FLAPPY_H
+#define FLAPPY_H
+
+UINT32 get_time();
+
+int align_back_buffer(UINT8 back_buffer[]);
+
+void run_game(UINT32 *base, UINT32 *back_base, UINT32 timeThen, UINT32 timeNow, UINT32 timeElapsed, Model model);
+#endif
