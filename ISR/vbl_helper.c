9f783dcfc4fbbc3087afcdb8e2862e8b1778151e
Rami Mikha <ramikmikha@gmail.com>
2024-03-31 22:52:48 -0600

    Updated VBL

--- ISR/vbl_helper.c
+++ ISR/vbl_helper.c
@@ -1,8 +1,29 @@
 #include "vbl_helper.h"
 
+int seconds = 0;
+int count = 0;
+int render_request = 0;
+
+/*This ISR is not being used in the game*/
 void do_VBL_ISR()
 {
+    count++;
+    if (count == 70){
+        seconds++;
+        count = 0;
+    }
 
+    if(seconds > NOTE_DURATION){
+        update_music(seconds);
+        seconds = 0;
+    }
+
+    bird_gravity();
+    pipe_move();
+    if (pass_pipe()){
+        pipe_spawn();
+    }
+    render_request = 1;
 }
 
 Vector install_vector(int num, Vector vector){
--- ISR/vbl_helper.h
+++ ISR/vbl_helper.h
@@ -1,11 +1,14 @@
 #ifndef VBL_HELPER_H
 #define VBL_HELPER_H
 
+#include "..\music\music.h"
+#include "..\events\events.h"
 
 #define VBL 28
 
 typedef void (*Vector) ();
 
+extern render_request;
 
 Vector install_vector(int num, Vector vector);
 void VBL_isr();
