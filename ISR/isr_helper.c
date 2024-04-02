4dd6ab43b937af5f5ad37c2aebd5e786e461cc8a
Rami Mikha <ramikmikha@gmail.com>
2024-04-01 18:41:56 -0600

    Created IKBD ISR Helper Functions

--- ISR/isr_helper.c
+++ ISR/isr_helper.c
@@ -4,6 +4,21 @@ int seconds = 0;
 int count = 0;
 int render_request = 0;
 
+
+
+volatile char * const IKBD_control = 0xFFFC00;
+volatile char * const IKBD_status = 0xFFFC00;
+volatile char * const MIDI_register = 0xFFFC04;
+volatile  SCANCODE * const IKBD_RDR = 0xFFFC02;
+volatile char * const MFP_B_REG = 0xFFFA11;
+
+int state = KEY;
+int mouse_x_value = 0, mouse_y_value = 0;
+int key_buffer[KEY_BUFFER_SIZE];
+int key_buffer_head = 0;
+int key_buffer_tail = 0;
+
+
 /*This ISR is not being used in the game*/
 /*void do_VBL_ISR()
 {
@@ -27,8 +42,56 @@ int render_request = 0;
 }*/
 
 
+void enqueue_key(int key){
+    key_buffer[key_buffer_tail] = key;
+    key_buffer_tail = (key_buffer_tail + 1) % KEY_BUFFER_SIZE;
+}
+
+int dequeue_key(){
+    int key = key_buffer[key_buffer_head];
+    key_buffer_head = (key_buffer_head + 1) % KEY_BUFFER_SIZE;
+    return key;
+}
+
+void disable_midi_interrupt(){
+    UINT32 old_ssp = Super(0);
+    *MIDI_register &= ~DATA_READY;
+    Super(old_ssp);
+}
+
+void enable_midi_interrupt(){
+    UINT32 old_ssp = Super(0);
+    *MIDI_register |= DATA_READY;
+    Super(old_ssp);
+}
+
+/*Mouse input not tested yet, and protection of critical sections not done*/
 void do_IKBD_ISR(){
-    
+    if(*IKBD_status & DATA_READY){
+        SCANCODE scancode = *IKBD_RDR;
+        switch (state){
+            case KEY:
+                if (scancode == MOUSE_INPUT){
+                    state = MOUSE_BUTTON;
+                }
+                else{
+                    enqueue_key(scancode);
+                }
+                break;
+            case MOUSE_BUTTON:
+                state = MOUSE_X;
+                break;
+            case MOUSE_X:
+                mouse_x_value = scancode;
+                state = MOUSE_Y;
+                break;
+            case MOUSE_Y:
+                mouse_y_value = scancode;
+                state = KEY;
+                break;
+        }
+        *MFP_B_REG &=  ~MFP_RECIEVE_ON;
+    }
 }
 
 Vector install_vector(int num, Vector vector){
--- ISR/isr_helper.h
+++ ISR/isr_helper.h
@@ -5,15 +5,32 @@
 #include "..\events\events.h"
 
 #define VBL 28
+#define IKBD 70
+
+#define DATA_READY 0x80
+#define MFP_RECIEVE_ON 0x40
+#define MOUSE_INPUT 0xF8
+#define KEY 0
+#define MOUSE_BUTTON 1
+#define MOUSE_X 2
+#define MOUSE_Y 3
+#define KEY_BUFFER_SIZE 256
 
 typedef void (*Vector) ();
+typedef UINT8 SCANCODE;
 
-extern render_request;
-
+extern int render_request;
+extern int key_buffer_head;
+extern int key_buffer_tail;
 Vector install_vector(int num, Vector vector);
 void VBL_isr();
 void do_VBL_ISR();
 void IKBD_isr();
 void do_IKBD_ISR();
+
+void enqueue_key(int key);
+int dequeue_key();
+void disable_midi_interrupt();
+void enable_midi_interrupt();
 
 #endif
